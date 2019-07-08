f9be73a3ee7dc3e4926ed1d1bf2bee21c6fa1a21
Loni Tra <lonitra@fb.com>
2019-07-08 16:46:14 -0700

    User sees instagram icon on home page

--- app/src/main/java/com/example/instagram/HomeActivity.java
+++ app/src/main/java/com/example/instagram/HomeActivity.java
@@ -1,13 +1,106 @@
 package com.example.instagram;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.example.instagram.model.Post;
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseFile;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
+
+import java.io.File;
+import java.util.List;
 
 public class HomeActivity extends AppCompatActivity {
+    private static final String imagePath = "./Desktop/smile.jpeg";
+
+    private EditText etDescription;
+    private Button btnCreate;
+    private Button btnRefresh;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        etDescription = findViewById(R.id.etDescription);
+        btnCreate = findViewById(R.id.btnCreate);
+        btnRefresh = findViewById(R.id.btnRefresh);
+
+        btnRefresh.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadTopPosts();
+            }
+        });
+
+        btnCreate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String description = etDescription.getText().toString();
+                final ParseUser user = ParseUser.getCurrentUser();
+                final File file = new File (imagePath); //would ask the user for their image here
+                final ParseFile parseFile = new ParseFile(file);
+                createPost(description, parseFile, user);
+            }
+        });
+
+
     }
+
+    private void createPost(String description, ParseFile imageFile, ParseUser user) {
+        final Post newPost = new Post();
+        newPost.setDescription(description);
+        newPost.setImage(imageFile);
+        newPost.setUser(user);
+
+        newPost.saveInBackground(new SaveCallback() {
+            @Override
+            public void done(ParseException e) {
+                if(e == null) {
+                    Log.d("CreatePost", "Post successful");
+                } else {
+                    Log.d("CreatePost", "Post failed");
+                    e.printStackTrace();
+                }
+            }
+        });
+
+    }
+
+    private void loadTopPosts() {
+        final Post.Query postQuery = new Post.Query();
+        postQuery.getTop().withUser();
+
+
+        postQuery.findInBackground(new FindCallback<Post>() {
+            @Override
+            public void done(List<Post> objects, ParseException e) {
+                if(e == null) {
+                    for (int i = 0; i < objects.size(); i++) {
+                        Log.d("HomeActivity", "Post[" + i + "] = " +objects.get(i).getDescription()
+                                + " username= " + objects.get(i).getUser().getUsername());
+                    }
+                } else {
+                    Log.d("HomeActivity", "Post load failed");
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.timeline, menu);
+        return true;
+    }
+
 }
---
+++ app/src/main/java/com/example/instagram/model/Post.java
@@ -0,0 +1,61 @@
+package com.example.instagram.model;
+
+import com.parse.ParseClassName;
+import com.parse.ParseFile;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+
+@ParseClassName("Post")
+public class Post extends ParseObject {
+    //keys as named in parse dashboard
+    private static final String KEY_DESCRIPTION = "description";
+    private static final String KEY_IMAGE = "image";
+    private static final String KEY_USER = "user";
+
+    public String getDescription() {
+        return getString(KEY_DESCRIPTION);
+    }
+
+    public void setDescription(String description) {
+        put(KEY_DESCRIPTION, description); //?? what does put do
+    }
+
+    public ParseFile getImage() {
+        return getParseFile(KEY_IMAGE);
+    }
+
+    public void setImage(ParseFile image) {
+        put(KEY_IMAGE, image);
+    }
+
+    //returns ParseUser so easier to display account username and avatar
+    public ParseUser getUser() {
+        return getParseUser(KEY_USER);
+    }
+
+    public void setUser(ParseUser user) {
+        put(KEY_USER, user);
+    }
+
+    public static class Query extends ParseQuery<Post> {
+
+        public Query() {
+            super(Post.class);
+        }
+
+        //?? don't quite understand this code
+        public Query getTop() {
+            setLimit(20);
+            return this;
+        }
+
+        public Query withUser() {
+            include("user");
+            return this;
+        }
+
+    }
+
+
+}

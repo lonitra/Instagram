62f8d223cd568f749405ac309ead5eb1f82b10dd
Loni Tra <lonitra@fb.com>
2019-07-09 17:06:47 -0700

    Added relative timestamp on post details

--- app/src/main/java/com/example/instagram/PostDetailActivity.java
+++ app/src/main/java/com/example/instagram/PostDetailActivity.java
@@ -5,6 +5,7 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.format.DateUtils;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -13,11 +14,15 @@ import com.parse.GetDataCallback;
 import com.parse.ParseException;
 import com.parse.ParseFile;
 
+import java.text.SimpleDateFormat;
+import java.util.Locale;
+
 public class PostDetailActivity extends AppCompatActivity {
 
     private ImageView ivImage;
     private TextView tvUsername;
     private TextView tvCaption;
+    private TextView tvDate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,11 +32,14 @@ public class PostDetailActivity extends AppCompatActivity {
         ivImage = findViewById(R.id.ivImage);
         tvUsername = findViewById(R.id.tvUsername);
         tvCaption = findViewById(R.id.tvCaption);
+        tvDate = findViewById(R.id.tvDate);
 
         Intent intent = getIntent();
         Post post = (Post) intent.getExtras().get("PostDetails");
 
+        tvUsername.setText(post.getUser().getUsername());
         tvCaption.setText(post.getDescription());
+        tvDate.setText(getRelativeTimeAgo(post.getCreatedAt().toString()));
         ParseFile file = post.getImage();
         file.getDataInBackground(new GetDataCallback() {
             @Override
@@ -40,5 +48,22 @@ public class PostDetailActivity extends AppCompatActivity {
                 ivImage.setImageBitmap(bitmap);
             }
         });
+    }
+
+    public String getRelativeTimeAgo(String rawJsonDate) {
+        String twitterFormat = "EEE MMM dd HH:mm:ss ZZZZZ yyyy";
+        SimpleDateFormat sf = new SimpleDateFormat(twitterFormat, Locale.ENGLISH);
+        sf.setLenient(true);
+
+        String relativeDate = "";
+        try {
+            long dateMillis = sf.parse(rawJsonDate).getTime();
+            relativeDate = DateUtils.getRelativeTimeSpanString(dateMillis,
+                    System.currentTimeMillis(), DateUtils.SECOND_IN_MILLIS).toString();
+        } catch (java.text.ParseException e) {
+            e.printStackTrace();
+        }
+
+        return relativeDate;
     }
 }
